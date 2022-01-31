32b91552ddef1687f461396ed1bb644dd2847fc0
Leonel Gonzalez <leogonzalez777.lg@gmail.com>
2022-01-31 16:29:22 -0300

    Borrado de seccion SingIn

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './styles.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import SingIn from './components/SingIn/SingIn';
-import { Boton } from './example/Boton/Boton';
+
 function App() {
   return (
     <>
-
       <NavBar/>
-  
       <ItemListContainer greeting={`En TechStore tenemos`}/>
-
-      {/* <SingIn/> */}
     </>
   );
 }
